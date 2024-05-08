1dc9e3cfc0265550b02c016fca03c7dd7f3f5d80
99kenny <helloiamkenny@gmail.com>
2024-05-08 19:07:23 +0900

    add: debug log

--- src/main/java/practice/communityservice/config/JwtAuthEntryPoint.java
+++ src/main/java/practice/communityservice/config/JwtAuthEntryPoint.java
@@ -18,6 +18,7 @@ public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
 
         // API Access without JWT token or invalid JWT
         if (errorCode == null || errorCode == ErrorCode.INVALID_JWT) {
+
             this.setResponse(response, ErrorCode.INVALID_JWT, "ACCESS TOKEN 만료");
             return;
         }
@@ -26,6 +27,8 @@ public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
         if (errorCode == ErrorCode.NEED_SIGN_IN) {
             this.setResponse(response, ErrorCode.NEED_SIGN_IN, "다시 로그인 해주세요.");
         }
+
+
     }
 
     private void setResponse(
