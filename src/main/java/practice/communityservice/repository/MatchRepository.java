2a93df0120670b2bb4c9cc5c0622527abcbed432
99kenny <helloiamkenny@gmail.com>
2024-06-20 16:26:39 +0900

    fix: getMatchList sql filter

--- src/main/java/practice/communityservice/repository/MatchRepository.java
+++ src/main/java/practice/communityservice/repository/MatchRepository.java
@@ -41,12 +41,13 @@ public class MatchRepository {
             return match;
         };
     };
-    public List<Match> getMatchList(int page, int pageSize) {
+    public List<Match> getMatchList(int page, int pageSize, LocalDateTime currentTime) {
         int pageStart = (page-1) * pageSize;
         String sql = "SELECT m.id AS id, m.title AS title, m.time AS time, m.address AS address, m.price AS price, m.info AS info, m.status AS status, m.link AS link, m.sex AS sex\n" +
-                "FROM `match` AS m\n" +
-                "ORDER BY m.time DESC LIMIT ?,?;";
-        return jdbcTemplate.query(sql, matchRowMapper(), pageStart, pageSize);
+                "FROM `match` AS m\n " +
+                "WHERE time >= ?\n" +
+                "ORDER BY m.time ASC LIMIT ?,?;";
+        return jdbcTemplate.query(sql, matchRowMapper(), currentTime, pageStart, pageSize);
     }
 
     public int allMatchCount() {
