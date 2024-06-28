05730db611812cccd402c30cfbe3ce20175fe13a
99kenny <helloiamkenny@gmail.com>
2024-06-28 18:40:11 +0900

    fix pwdvalidator

--- src/main/java/practice/communityservice/domain/validation/EmailPasswordMatchValidator.java
+++ src/main/java/practice/communityservice/domain/validation/EmailPasswordMatchValidator.java
@@ -12,14 +12,13 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Slf4j
 public class EmailPasswordMatchValidator extends AbstractValidator{
-    private final PasswordEncoder passwordEncoder;
     private final String destPassword;
     private final String srcPassword;
     @Override
     public void validate() {
         log.debug("ORIGIN PWD={}",destPassword);
         log.debug("SRC PWD={}",srcPassword);
-        if(!destPassword.equals(passwordEncoder.encode(srcPassword))){
+        if(!destPassword.equals(srcPassword)){
             throw new BadRequestException(
                     ErrorCode.INVALID_USER_DATA_REQUEST,
                     "비밀번호가 틀립니다."
